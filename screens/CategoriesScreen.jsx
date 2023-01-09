0a739fcb5ccaab8f7805ac2f0331cfdf99041d0e
Gerard Siles <gerardsiles@gmail.com>
2023-01-09 18:19:42 +0000

    first commit

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,28 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+const CategoriesScreen = ({ navigation }) => {
+	function renderCategoryItem(itemData) {
+		function onPressHandler() {
+			navigation.navigate('MealsOverview');
+		}
+
+		return (
+			<CategoryGridTile
+				title={itemData.item.title}
+				color={itemData.item.color}
+				onPress={onPressHandler}
+			/>
+		);
+	}
+	return (
+		<FlatList
+			data={CATEGORIES}
+			keyExtractor={item => item.id}
+			renderItem={renderCategoryItem}
+			numColumns={2}
+		/>
+	);
+};
+export default CategoriesScreen;
